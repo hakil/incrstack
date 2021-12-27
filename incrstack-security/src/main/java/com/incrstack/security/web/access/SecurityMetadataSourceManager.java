58e45b0468db90b040d5e7929944151422f4c742
李世奇 <lishiqi@zhidianlife.com>
2021-12-27 17:59:22 +0800

    todo

---
+++ incrstack-security/src/main/java/com/incrstack/security/web/access/SecurityMetadataSourceManager.java
@@ -0,0 +1,11 @@
+package com.incrstack.security.web.access;
+
+import org.springframework.security.access.SecurityMetadataSource;
+
+/**
+ * @author Hakil
+ * @date 2021/12/27 17:11
+ */
+public interface SecurityMetadataSourceManager extends SecurityMetadataSource {
+
+}
